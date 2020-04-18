93c3f8c612d13c092f4aeb8bd0a4da15758471aa
john <john4pius@gmail.com>
2020-04-18 03:27:24 +0100

    connect db + initiate db migration

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgres://demorole1:password1@localhost:5432/fyyur'
